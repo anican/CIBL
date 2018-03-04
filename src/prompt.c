de7e5ca4aad5959b232f1e1abf7ded01b0d004a4
canuani <anirudhcan@outlook.com>
2018-03-03 21:00:54 -0800

    Added functionality for prompt on Windows

--- src/prompt.c
+++ src/prompt.c
@@ -1,10 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
+/* Compile following functions if operating on Windows */
 
-/* basic read-evaluate-print-loop (REPL) that echoes input */ 
-/* buffer for storing user input */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {
+}
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {
+}
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {
+}
+
+
+/* Else compile for OSX/Linux using editline headers */
+#else 
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
     /* Print Version and Exit Information */
@@ -29,4 +85,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
---
+++ src/prompt_unix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
+/* basic read-evaluate-print-loop (REPL) that echoes input */ 
+/* buffer for storing user input */
+
+int main(int argc, char** argv) {
+    /* Print Version and Exit Information */
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press C-c to Exit\n");
+
+    /* Infinite Loop */
+    while (1) { 
+        
+        char* input = readline("lispy> ");  
+
+        /* Output our argument */
+        add_history(input);
+         
+        /* Echo input back at user */
+        printf("Echo: %s\n", input);
+        
+        /* Free retrieved input */
+        free(input);
+        
+    }
+
+    return 0;
+}
+
